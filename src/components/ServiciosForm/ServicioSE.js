8be290cbd2e0f23ca771f958c986af7067e080f4
Enrique Koyoc <kikekc10@gmail.com>
2020-09-25 16:29:30 -0500

    se agrega comentario en la plantilla de correo

--- src/components/ServiciosForm/ServicioSE.js
+++ src/components/ServiciosForm/ServicioSE.js
@@ -59,6 +59,7 @@ const ServicioSEs = ({idG, client, setActiveStep}) =>{
                   timer: 1500
                 }).then(function() {
 
+                // Plantilla del correo electronico que se envía al cliente cuando es dado de alta una orden de servicio
                 const html= `<table style="max-width: 600px; padding: 10px; margin:0 auto; border-collapse: collapse;">
                     <tr>
                         <td style="background-color: #ecf0f1; text-align: left; padding: 0">
